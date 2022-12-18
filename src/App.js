b6eef6b96a4e09e61e9fd2df885bece3f09f8583
Davit Vardanyan <jedavard@gmail.com>
2022-12-18 22:48:01 +0400

    init

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import i from './i4.jpg'
+import j from './j1.jpg'
 
 function App() {
   const [count, setCount] = React.useState("12:05:21");
+  const [nextPage, setNextPage] = React.useState(false);
   setTimeout(() => {
     setCount(new Date().toLocaleTimeString().slice(0, 8));
   }, 1000);
   return (
     <div className="App">
-      <img src={i} alt className="Image" />
-      <div className="Dates">2022-12-19  {count}</div>
+      {nextPage ? ( <>
+        <img src={j} alt className="Image" />
+        <div className="asdasd2" onClick={() => setNextPage(false)} />
+      </>) : (<>
+        <img src={i} alt className="Image" />
+        <div className="Dates">2022-12-19  {count}</div>
+        <div className="asdasd" onClick={() => setNextPage(true)} /></>)}
     </div>
   );
 }
