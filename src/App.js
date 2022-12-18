20b20629434cd128ca1c88f969b88cb33b0d9da1
Davit Vardanyan <jedavard@gmail.com>
2022-12-18 22:27:46 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import i from './i2.jpg'
+import i from './i4.jpg'
 
 function App() {
+  const [count, setCount] = React.useState("12:05:21");
+  setTimeout(() => {
+    setCount(new Date().toLocaleTimeString().slice(0, 8));
+  }, 1000);
   return (
     <div className="App">
       <img src={i} alt className="Image" />
+      <div className="Dates">2022-12-19  {count}</div>
     </div>
   );
 }
